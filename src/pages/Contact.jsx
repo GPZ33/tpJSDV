eee2998a6bc10e57c44a9fc5207379966717c9b9
gpz33 <remi.pedeflous@gmail.com>
2023-12-11 11:01:12 +0100

    Added form contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,10 +2,31 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const Contact = () => {
+
+  const handleContactSubmit = (event) => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+
+    console.log(email,message);
+  }
+
   return (
     <>
       <Header />
       <p>Page contact</p>
+      <form onSubmit={handleContactSubmit}>
+        <label>
+          Email
+          <input type="text" name="email" />
+        </label>
+        <label>
+          Message
+          <input type="text" name="message" />
+        </label>
+        <input type="submit" />
+      </form>
       <Footer />
     </>
   );
